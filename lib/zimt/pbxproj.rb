fe53c4d367954e25145e9fec0f675165e6145991
Martin Schürrer <martin@schuerrer.org>
2011-08-10 01:53:08 +0200

    Add @tjw's documentation on PBXObjectId

--- lib/zimt/pbxproj.rb
+++ lib/zimt/pbxproj.rb
@@ -4,6 +4,27 @@ module Zimt
       `plutil -convert json -o - #{file}`
     end
 
+
+    # Documentation on PBXObjectId by @tjw
+    # http://lists.apple.com/archives/projectbuilder-users/2003/Jan/msg00263.html
+    #
+    # ObjectId gets generated in the following format:
+    # +----------------------------------------------------------------------+
+    # | RANDOM    | SEQ       |  TIME                | PID       | IP (2)    |
+    # | byte byte | byte byte | byte byte  byte byte | byte byte | byte byte |
+    # +----------------------------------------------------------------------+
+    # RANDOM = 2 bytes of random number for distribution
+    # SEQ    = Unsigned short sequence counter that starts randomly.
+    # TIME = Seconds since epoch (1/1/1970)
+    # PID  = Process ID
+    #        This is only two bytes even though most pids are longs.
+    #        Here you would take the lower 2 bytes.
+    # IP   = IP Address of the machine (subnet.hostId)
+    def self.uuid
+      # TODO
+      rand(16**24).to_s(16).upcase
+    end
+
     attr_reader :hash, :objects, :root
 
     def initialize(file)
