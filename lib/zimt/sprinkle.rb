2ae97d235d155cf0fdb2414e9f48745b92964b4f
Martin Schürrer <martin@schuerrer.org>
2011-09-08 19:15:45 +0200

    Fix syntax error

--- lib/zimt/sprinkle.rb
+++ lib/zimt/sprinkle.rb
@@ -31,7 +31,7 @@ module Zimt
         end
         if file.end_with? ".m"
           Zimt.pbxproj.add_m_file(file)
-        else if file.end_with? ".h"
+        elsif file.end_with? ".h"
           Zimt.pbxproj.add_h_file(file)
         else
           Zimt.pbxproj.add_resource_file(file)
